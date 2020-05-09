f74d5120300f8cd162fe3c754816b30793fb17fd
jparkorea <gkshdn@naver.com>
2020-05-09 20:29:33 +0900

    Updates

---
+++ precache-manifest.cc22cc8d6cea8ebe0bbfd06d67a48e83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6248bf54157e4839a8a7ff0d2e2cd7d",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "c94359b87809feffa65e",
+    "url": "/movie_app/static/css/main.fa7ed925.chunk.css"
+  },
+  {
+    "revision": "7506d3f18b729dcf158e",
+    "url": "/movie_app/static/js/2.64f82b83.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.64f82b83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c94359b87809feffa65e",
+    "url": "/movie_app/static/js/main.e9b3c39a.chunk.js"
+  },
+  {
+    "revision": "ffbd4de4896f1a599175",
+    "url": "/movie_app/static/js/runtime-main.0186e20d.js"
+  }
+]);
